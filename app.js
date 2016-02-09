6ef7ee51e612134021bb2450a2467b0cdc0ee4a6
Alejandro <alejandroj1234@gmail.com>
2016-02-09 00:32:25 -0500

    trying to fix heroku

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + "/public/"));
+app.listen(process.env.PORT || 5000);
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
